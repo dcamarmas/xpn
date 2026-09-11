81557f7e5f4db50ce3a3233b94344965c948af6e
Diego Camarmas-Alonso <dcamarma@inf.uc3m.es>
2026-09-11 12:51:13 +0200

    bypass minor update

--- src/bypass/xpn_bypass.c
+++ src/bypass/xpn_bypass.c
@@ -3002,7 +3002,7 @@
       value = getenv("XPN_IS_MPI_SERVER");
       if (NULL == value)
       {
-        debug_info("[BYPASS] << After MPI_Init_thread dentro\n");
+        debug_info("[BYPASS] << After MPI_Init_thread\n");
         return PMPI_Init_thread( argc, argv, required, provided );
       }
       else
